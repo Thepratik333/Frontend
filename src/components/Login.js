033f291b936fb1c8f59beaeabd62647840de5654
Thepratik333 <110605777+Thepratik333@users.noreply.github.com>
2024-03-11 01:03:59 +0530

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -12,7 +12,7 @@ const Login = () => {
     let navigate = useNavigate()
     const handleSubmit = async (e) => {
         e.preventDefault()
-        const response = await fetch("https://backend-4f7l.onrender.com:10000/api/auth/login", {
+        const response = await fetch("https://backend-4f7l.onrender.com/api/auth/login", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
